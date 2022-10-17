7015ab0eaafec03eeed7f2a7a80a279610be8f11
RK <robertkozak97@gmail.com>
2022-10-17 14:00:48 +0200

    Move funcitons to other file - more readable code

---
+++ interface.py
@@ -0,0 +1,10 @@
+import math
+
+# Function for denormalization joint coordinates
+def denormalization(hand, hand_joint_num, img_height, img_width):
+    normalized_x = hand.landmark[hand_joint_num].x 
+    normalized_y = hand.landmark[hand_joint_num].y
+    denorm_x = int(min(math.floor(normalized_x * img_width), img_width - 1))
+    denorm_y = int(min(math.floor(normalized_y * img_height), img_height - 1))
+
+    return (denorm_x, denorm_y)
