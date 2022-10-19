b6805dbc68388c0e03552ee42941de8527bcce2c
RK <robertkozak97@gmail.com>
2022-10-19 09:53:43 +0200

    Add to list, and remember first point

--- interface.py
+++ interface.py
@@ -10,3 +10,17 @@ def denormalization(hand, hand_joint_num, img_height, img_width):
     denorm_y = int(min(math.floor(normalized_y * img_height), img_height - 1))
 
     return (denorm_x, denorm_y)
+
+def add_to_list(coord_list, hand):
+    try:
+        if len(coord_list) < 10:
+            coord_list.append([hand.landmark[8].x, hand.landmark[8].y])
+                            
+        else:                    
+            coord_list.pop(0)
+            coord_list.append([hand.landmark[8].x, hand.landmark[8].y])
+    except NameError:
+        coord_list = [[hand.landmark[8].x, hand.landmark[8].y]]
+    return coord_list   
+     
+        
